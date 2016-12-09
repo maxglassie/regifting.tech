2677185cc26f2259c30fe88aac6ad802ee6f7bf2
Mike Schutte <tmikeschutte@gmail.com>
2016-12-08 20:44:44 -0700

    complete iteration 1 for registery

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,23 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Regifter.create(name: "",
-                gift: "",
-                description: "",
-                avatar: "",
-                lat: "",
-                long: "")
+Regifter.create(name: "Donald Trump",
+               gift: "The Presidency",
+               description: "office job - I can't believe they gave it to me",
+               avatar: "http://mediamass.net/jdd/public/documents/celebrities/858.jpg",
+               lat: "39.7392",
+               long: "-104.9903")
+
+Regifter.create(name: "Aunt Sally",
+               gift: "fruitcake",
+               description: "nutritious, delicious, and from the 70s",
+               avatar: "https://0ut0fthebox.files.wordpress.com/2013/12/aunt_conciera_jvs_aunt_from_the_old_country.jpg",
+               lat: "39.750692",
+               long: "-104.99006")
+
+Regifter.create(name: "little kid at the park",
+               gift: "chewing gum",
+               description: "reduce, reuse, regift",
+               avatar: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTynMRcm5bMAlh_VqsTeW2RqamKnTc6i49PioXisDewXhx49LUO9g",
+               lat: "39.758079",
+               long: "-104.9703")
